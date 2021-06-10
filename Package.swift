850ea472ed83fc50dc62024719d58d1dcab4160a
Cătălin Stan <catalin.stan@me.com>
2021-06-10 12:45:49 +0200

    Add boilerplate SPM support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ObjcClassEnum",
+    platforms: [
+        .macOS(.v10_11),
+    ],
+    products: [
+        .library(name: "ObjcClassEnum",
+                 targets: ["ObjcClassEnum"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ObjcClassEnum",
+            dependencies: [],
+            path: "./",
+            exclude: [ "./TestApp"],
+            publicHeadersPath: "include",
+            cSettings: [
+//                .headerSearchPath("Source"),
+            ]
+        ),
+    ]
+)
